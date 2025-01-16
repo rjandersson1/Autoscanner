07f57e5f98a1ea9bb9167d05c74d342c9b1c7c9f
rjandersson1 <rjandersson.1@gmail.com>
2025-01-16 13:40:57 +0100

    temporarily changed getMap() to improve speed
    changed from self-dev mapFloat() func to arduino.h map() to hopefully speed up runtime

--- Arduino/main/buttons.cpp
+++ Arduino/main/buttons.cpp
@@ -46,7 +46,8 @@ float Poti::getFloat() {
 
 // Returns custom mapping
 float Poti::getMap() {
-	float value = mapFloat(analogRead(pin), min_value, max_value, mapMin, mapMax);
+	// float value = mapFloat(analogRead(pin), min_value, max_value, mapMin, mapMax);
+	float value = map(analogRead(pin), min_value, max_value, mapMin, mapMax);
 	return value;
 }
 
