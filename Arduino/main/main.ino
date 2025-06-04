9d77147ee8512c2cc4d653196c65c3abfaab3cb7
rjandersson1 <rjandersson.1@gmail.com>
2025-06-04 16:56:51 +0200

    General + Library Cleanup + TMC2209 implementation
    - moved libraries to .../main/libraries/
    - redefined pins according to hardware as of may 2025
    - began TMC2209 implementation

--- Arduino/main/main.ino
+++ Arduino/main/main.ino
@@ -19,41 +19,46 @@
 
 // ======================== Libraries =========================== //
 #include <Arduino.h>
-#include <IRremote.h>
-// #include "stepper.h"
-#include "buttons.h"
-#include "filmScanner.h"
+#include <SoftwareSerial.h>
+#include "libraries/buttons.h"
+#include "libraries/filmScanner.h"
+#include "libraries/TMC/TMCStepper.h"
+#include "libraries/IRremote/IRremote.h"
 
 // ======================== Pindef ============================= //
 
-#define PIN_MS1  13  // YL -> MS1
-#define PIN_EN   12  // GR -> EN
-#define PIN_DIR  11  // OR -> DIR
-#define PIN_STEP 10  // RD -> STEP
-#define PIN_IR   9   // BR -> IR LED
+#define PIN_IR      12
+#define PIN_LED     8
 #define IR_SEND_PIN PIN_IR // Set the IR sending pin to Pin 9
-#define PIN_SLP  8   // BR -> SLP
-#define PIN_RST  7   // BK -> RST
-#define PIN_MS3  6   // WH -> MS3
-#define PIN_MS2  5   // GY -> MS2
-#define PIN_BTN_A 4  // GY -> BUTTON A
-#define PIN_BTN_B 3  // WH -> BUTTON B
-#define PIN_BTN_C 2  // BK -> BUTTON C
-#define PIN_POTI A0  // RD -> POTI
+#define PIN_BTN_A   A3  
+#define PIN_BTN_B   A0  
+#define PIN_BTN_C   A5  
+#define PIN_POTI    A6  
+#define SW_DN       A1
+#define SW_UP       A2
+
+// Define motor driver pins (TMC2209)
+#define PIN_EN     2
+#define PIN_STEP   3
+#define PIN_DIR    4
+#define R_SENSE    0.11f
+#define DRIVER_ADDRESS 0b00 // MS1/MS2 set to GND (0b00)
+#define PIN_TMC2209_RX 5 // D5 goes to TMC2209 TX (PDN_UART)
+#define PIN_TMC2209_TX 6 // D6 goes to TMC2209 RX (PDN_UART)
 
 // ===================== Button Callback Definition =================//
 
-#define FUN_A1 scanner.dynamicMove()
+#define FUN_A1 scanner.takePhoto() // Take photo
 #define FUN_A2 
 #define FUN_A3 
 #define FUN_AH 
 #define TIME_AH 1000    // Hold time [ms]
 #define TIME_A 0        // Multiclick time [ms]
 
-#define FUN_B1
+#define FUN_B1 
 #define FUN_B2 
 #define FUN_B3 
-#define FUN_BH stepper.cycleMicrostepMode()
+#define FUN_BH 
 #define TIME_BH 1000    // Hold time [ms]
 #define TIME_B 0        // Multiclick time [ms]
 
@@ -70,28 +75,21 @@ Button buttonA(PIN_BTN_A);
 Button buttonB(PIN_BTN_B);
 toggleButton buttonC(PIN_BTN_C);
 Poti poti(PIN_POTI, 0, 1023);
-IRsend ir(IR_SEND_PIN); // IR object
-Timer timer(); // timer object
+IRsend ir(IR_SEND_PIN);
 
-// Stepper
-#include "A4988.h"
-#define MOTOR_STEPS 200 // default steps/rev
-#define RPM 1250 // cruise speed RPM
-#define MOTOR_ACCEL 16000 // [STEP/s]
-#define MOTOR_DECEL 9999999 // [STEP/s]
-#define MICROSTEPS 16 // Microstep mode (hardwired)
-A4988 stepper(MOTOR_STEPS, PIN_DIR, PIN_STEP, PIN_EN, PIN_MS1, PIN_MS2, PIN_MS3);
+// Stepper motor
+SoftwareSerial tmc_serial(PIN_TMC2209_RX, PIN_TMC2209_TX); // RX, TX
+TMC2209Stepper stepper(&tmc_serial, R_SENSE, DRIVER_ADDRESS);
 
 // Film scanner
-filmScanner scanner(stepper, buttonA, buttonB, buttonC, poti);
+filmScanner scanner(stepper, buttonA, buttonB, buttonC, poti, ir);
 
 // ======================== Main ============================= //
 
 void setup() {
-	Serial.begin(115200);
-	setupButtons();
-	setupStepper();
-	Serial.println("init...");
+	Serial.begin(9600);
+	initButtons();
+	initStepper();
 }
 
 void loop() {
@@ -106,7 +104,7 @@ void readButtons() {
 	poti.read();
 }
 
-void setupButtons() {	
+void initButtons() {	
   // Button A
   buttonA.onSingleClick([] { FUN_A1; });
   buttonA.onDoubleClick([] { FUN_A2; });
@@ -130,10 +128,13 @@ void setupButtons() {
   buttonC.toggledOff([] { FUN_C_OFF; });
 }
 
-void setupStepper() {
-	stepper.begin(RPM, MICROSTEPS);
-	stepper.setEnableActiveState(LOW); // Enabled when PIN_EN LOW
-	stepper.setSpeedProfile(stepper.LINEAR_SPEED, MOTOR_ACCEL, MOTOR_DECEL); // set acceleration profile
-	stepper.setMicrostep(1);
-	stepper.enable();
+// Initializes the TMC2209 stepper driver
+void initStepper() {
+  tmc_serial.begin(115200); // Initialize UART communication
+  stepper.begin();
+  stepper.toff(4);
+  stepper.blank_time(24);
+  stepper.rms_current(100);
+  stepper.microsteps(1);
+  stepper.pwm_autoscale(true);
 }
